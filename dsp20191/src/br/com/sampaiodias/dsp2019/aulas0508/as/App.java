9cb3b1131703f2c25a44b83d59f45c4a820fe130
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2019-03-24 20:02:57 -0300

    Leitura do arquivo dat

--- dsp20191/src/br/com/sampaiodias/dsp2019/aulas0508/as/App.java
+++ dsp20191/src/br/com/sampaiodias/dsp2019/aulas0508/as/App.java
@@ -5,13 +5,49 @@
  */
 package br.com.sampaiodias.dsp2019.aulas0508.as;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 /**
  * Resolve a Atividade Supervisionada - aulas0508.
  * @author Lucas Sampaio Dias
  */
 public class App {
-    public static void main(String[] args) {
-        Aluno a = new Aluno("João", 123, 7, 5);
-        System.out.println(a.toString());
+    public static void main(String[] args) throws IOException {
+        ArrayList<Aluno> alunos = getAlunosNoArquivo("avaliacao.dat");
+        alunos.forEach((aluno) -> {
+            System.out.println(aluno.toString());
+        });
     }
+    
+    public static ArrayList<Aluno> getAlunosNoArquivo(String arquivo) 
+            throws IOException {
+        BufferedReader br = App.getConteudo("src/main/resources/" + arquivo);
+        
+        ArrayList<Aluno> alunos = new ArrayList<>();
+        String linha;
+        
+        while ((linha = br.readLine()) != null) {
+            if (linha.length() > 0) {
+                StringTokenizer token = new StringTokenizer(linha, ",");
+                Aluno a = new Aluno(token.nextToken(),
+                        Long.parseLong(token.nextToken()), 
+                        Double.parseDouble(token.nextToken()), 
+                        Double.parseDouble(token.nextToken()));
+                alunos.add(a);
+            }
+        }
+        
+        return alunos;
+    }
+    
+    private static BufferedReader getConteudo(String caminho) throws FileNotFoundException, IOException {
+        FileReader fileReader = new FileReader(caminho);
+        BufferedReader br = new BufferedReader(fileReader);
+        return br;
+    }  
 }
